7ae9084d26c7e62f7a5fcf07ae3abff8c3c787a7
prashant.tiwari <prashant.tiwari@blacklane.com>
2021-03-27 23:45:01 +0100

    adding initial code for search query

--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
+	"net/url"
 	"os"
+	"log"
+	"fmt"
+	"github.com/joho/godotenv"
 )
 
+var tpl = template.Must(template.ParseFiles("index.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
+	//w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, nil)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchQuery)
+	fmt.Println("Page is: ", page)
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
-	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("assets"))
 
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/search", searchHandler)
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
